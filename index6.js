761e444c16bc3f7afb593f06cd27e36f16fcddf8
HRXT1000 <70828069+HRXT1000@users.noreply.github.com>
2020-12-20 15:50:08 -0500

    Add files via upload

---
+++ index6.js
@@ -0,0 +1,88 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+export default class Login extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            user: '',
+            password: '',
+            error: false
+        };
+    }
+
+    submitFormRequest() {
+        const { user, password } = this.state;
+
+        if(user && password) {
+            axios
+            .post('/signin', { user, password })
+            .then(res => {
+                const { data } = res;
+                const { verified, id } = data;
+                if (verified) {
+                    this.props.history.push({
+                        pathname: '/dashboard', 
+                        state: { user, userId: id }
+                    });
+                }
+            })
+            .catch(err => {
+                console.log(err, 'ERROR-AT-LOGIN');
+                this.setState({error: true});
+            });
+        } 
+    }
+
+    handleUserInput(event) {
+        this.setState(
+            { user: event.target.value }
+        );
+    }
+
+    handlePasswordInput(event) {
+        this.setState(
+            { password: event.target.value }
+        );
+    }
+
+    render() {
+        const { user, password } = this.state;
+
+        return (
+            <div className="login-container">
+                <div className="login-form-container">
+                    {
+                        !this.state.error ? 
+                        (<h2>Login or Sign up!
+                            <span className="mini-header-under"></span>
+                        </h2>) : 
+                        (<h2 className="form-login-error">Error! Try again!
+                            <span className="mini-header-under-error"></span>
+                        </h2>)
+                    }
+                    <form name="loginForm" onSubmit={() => this.submitFormRequest()}>
+                        <div>
+                            <div className="input-container">
+                                <div>
+                                    <span>Username</span>
+                                </div>
+                                <input type="text" name="user" value={user} onChange={(event) => this.handleUserInput(event)}/>
+                            </div>
+                            <div className="input-container">
+                                <div>
+                                    <span>Password</span>
+                                </div>
+                                <input type="password" name="password" value={password} onChange={(event) => this.handlePasswordInput(event)}/>
+                            </div>
+                        </div>
+                        <div className="form-submit-button">
+                            <span onClick={() => this.submitFormRequest()}>SUBMIT</span>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        );
+    } 
+}
