9549347d66ae67f87345c671200833b718731b1b
HRXT1000 <70828069+HRXT1000@users.noreply.github.com>
2020-12-20 13:13:44 -0500

    Add files via upload

---
+++ photo-config.js
@@ -0,0 +1,12 @@
+export default {
+    sports: '/static/images/sports.jpg',
+    lifestyle: '/static/images/lifestyle.jpg',
+    politics: '/static/images/politics.jpg',
+    fashion: '/static/images/fashion.jpg',
+    architecture: '/static/images/architecture.jpg',
+    local: '/static/images/local.jpg',
+    eats: '/static/images/eats.jpg',
+    home:'/static/images/home.jpg',
+    finance: '/static/images/finance.jpg',
+    fallback: '/static/images/fallback.jpg'
+}
